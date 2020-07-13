fb5ca9e262eff6a0e613cdfdea5f41d1140480a6
Jason Kruser <yokariig@gmail.com>
2020-07-13 09:30:24 -0400

    first push

---
+++ index.php
@@ -0,0 +1,197 @@
+<!-- Développé avec <3 par Omar Boulbaze -->
+
+<html>
+
+<head>
+
+<meta charset="utf-8" />
+
+
+<!-- OG meta -->
+<meta property="og:site_name" content="www.tcorner.net">
+<meta property="og:url" content="https://www.tcorner.net/">
+<meta property="og:title" content="TCorner®">
+<meta property="og:description" content="La perfection au cœur de nos valeurs">
+<meta property="og:type" content="website">
+<meta name="og:image" content="./image/logo.jpg">
+
+<!-- STYLES -->
+<link rel="stylesheet" type="text/css" href="./0bootstrap/bootstrap.min.css"/>
+<link href="./0fontawesome/css/all.css" rel="stylesheet" />
+<link href="./style/styleIndex.css" rel="stylesheet" />
+
+<!-- FAVICON -->
+<link rel="shortcut icon" src="./image/favicon.png" type="image/png" href="#"/>
+
+<!-- FONTS -->
+<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+
+<!-- W3 ANIMATIONS -->
+<link rel="stylesheet" href="./0w3/w3.css">
+
+<!-- TITLE -->
+<title>Accueil · TCorner®</title>
+           
+</head>
+
+
+<body id="page-top">
+
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">TCorner®</a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Découverte</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Équipe</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projet">Notre projet</a></li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+
+
+
+        <!-- PAGE PRINCIPALE-->
+        <header class="masthead">
+            <div class="container d-flex h-100 align-items-center">
+                <div class="mx-auto text-center">
+                    <h1 class="mx-auto my-0">Technological Corner</h1>
+                    <h3 class="text-white-50 mx-auto mt-5 ">TCorner® - La perfection au cœur de nos valeurs</h3>
+                    <h6 class="text-white-50 mx-auto  mb-5">Nous sommes une entreprise informatique privée internationale spécialisée dans
+                     le domaine de gestion et d’organisation d’informations professionnelles afin de les récupérer,
+                      trier et enfin les classer dans le but de faciliter la récupération et le traitement de données.</h6>
+                    <a class="btn btn-outline-light border decouverte js-scroll-trigger" href="#about">COMMENCER MA DÉCOUVERTE</a>
+                </div>
+            </div>
+        </header>
+
+
+        <!-- NOTRE HISTOIRE  -->
+
+
+        <section class="about-section text-center" id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <h2 class="text-white mb-4">Notre histoire</h2>
+                        <p class="text-white-50">Fondée en 1967, TCorner™ s’est particulièrement imposée et s’est démarquée
+                         des autres entreprises par son expertise,sa rigueur et sa méthodologie de travail grâce à la qualité 
+                         du service fourni. </p>
+                    </div>
+                </div>
+                <img class="img-fluid" src="https://cdn6.findmyshift.com/O5I004B82HB9707AKIM36CK75E2G4DA6U8UlI4DKqM8D6N8M6D92BDKE09B77N7" alt="" />
+            </div>
+        </section>
+       
+       <!-- Notre équipe -->
+<div class="container-fluid" id="team">
+  <div class="text-center p-2">
+    <h1>Notre équipe</h1>
+  </div>
+      <div class="row mb-5">
+
+      <div class="col-lg-3 col-12">
+      
+              <div class="bg-light text-center hosting overflow-hidden imgOverflow">  
+                  <div class="my-3 ">
+                  <h2 class="display-5">Omar Boulbaze</h2>
+                  <p class="lead">Chef d'équipe</p>
+                  </div>
+                  <img class="img-fluid" src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortWaved&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=PastelGreen&eyeType=Close&eyebrowType=Default&mouthType=Twinkle&skinColor=Light" >
+              </div>
+              
+          </div>
+
+          <div class="col-lg-3 col-12">
+      
+      <div class="bg-light text-center hosting overflow-hidden imgOverflow">  
+          <div class="my-3 ">
+          <h2 class="display-5">Yves Kolie</h2>
+          <p class="lead">Web Master</p>
+          </div>
+          <img class="img-fluid" src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortRound&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=White&eyeType=Default&eyebrowType=Default&mouthType=Twinkle&skinColor=Black" >
+      </div>
+      
+  </div>
+         
+
+  <div class="col-lg-3 col-12">
+      
+              <div class="bg-light text-center hosting overflow-hidden imgOverflow">  
+                  <div class="my-3 ">
+                  <h2 class="display-5">Kamilia Sara Sbahi</h2>
+                  <p class="lead">Programmeuse</p>
+                  </div>
+                  
+                  <img class="img-fluid" src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraightStrand&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Wink&eyebrowType=Default&mouthType=Default&skinColor=Light" >
+              </div>
+              
+          </div>
+
+         
+
+          <div class="col-lg-3 col-12">
+      
+      <div class="bg-light text-center hosting overflow-hidden imgOverflow">  
+          <div class="my-3 ">
+          <h3 class="display-5">Mame Diarra Bousso Ndiaye</h3>
+          <p class="lead">Analyste</p>
+          </div>
+          <img class="img-fluid" src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=PastelRed&eyeType=Happy&eyebrowType=Default&mouthType=Twinkle&skinColor=Black" >
+      </div>
+      
+  </div>
+          
+          
+          
+          
+        
+      </div>
+</div>
+       
+<section class="signup-section bigMarginTop" id="projet">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto text-center">
+            
+            <h2 class="text-white mb-4">En effet, l’entreprise a conçu plus de 5 000 logiciels et progiciels informatiques pour des gouvernements et des entreprises privées à travers le monde. 
+
+Situés à Montréal au Québec, l’équipe compte 1384 employés à nos jours, comprends 80 départements et ne cesse de s’élargir. </h2>
+            <a class="btn btn-outline-light border decouverte js-scroll-trigger"  href="#" ><i class="fas fa-arrow-up"></i></a>
+    </div>
+</div>
+</div>
+</section>
+        
+       
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50">
+          
+        
+        <div class="container">Copyright © TCorner 2020</div>
+
+  
+      
+      </footer>
+      
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Script perso -->
+        <script src="./script/scriptIndex.js"></script>
+
+        
+      
+        
+    </body>
+</html>
---
+++ script/script.js
@@ -0,0 +1,27 @@
+// Countdown
+
+var countDownDate = new Date("Jul 4, 2020 15:00:00").getTime();
+
+
+var x = setInterval( ()=> {
+
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("countdown").innerHTML = days + " jours " + hours + " heures "
+  + minutes + " minutes " + seconds + " secondes ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
